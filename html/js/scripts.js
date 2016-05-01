98c8fb47260d070679e60235e3d465e34e3c930a
crazyyy <crazyyy@gmail.com>
2016-05-01 16:56:19 +0300

    relise

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,10 +1,2 @@
 !function(){for(var e,n=function(){},o=["assert","clear","count","debug","dir","dirxml","error","exception","group","groupCollapsed","groupEnd","info","log","markTimeline","profile","profileEnd","table","time","timeEnd","timeline","timelineEnd","timeStamp","trace","warn"],i=o.length,r=window.console=window.console||{};i--;)e=o[i],r[e]||(r[e]=n)}();
-
-
-
-$('.leftside a').on('click', function(event) {
-  if ( $(this).parent('li').has('.sub-menu').length > 0  ) {
-    event.preventDefault();
-    $(this).parent('li').toggleClass('display-sub-menu')
-  }
-})
+$('.leftside a').on('click',function(event){if($(this).parent('li').has('.sub-menu').length>0){event.preventDefault();$(this).parent('li').toggleClass('display-sub-menu')}})
